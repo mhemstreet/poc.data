7797967ec838133e8cba1c1d2a8c08b44772f897
veracity <veracity@VS2015C>
2017-08-25 21:35:46 +0000

    autofac config

--- poc.repo/GenericRepository.cs
+++ poc.repo/GenericRepository.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace poc.repo
 {
-    public class Class1
+    public class GenericRepository
     {
+
     }
 }
---
+++ poc.repo/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using poc.data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poc.repo
+{
+    public interface IRepository<T> where T: IBaseEntity
+    {
+        IEnumerable<T> GetAll();
+        T Get(Guid Id);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void SaveChanges();
+    }
+}
